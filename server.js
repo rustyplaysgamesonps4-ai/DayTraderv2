1f213b75ed687fc85c34008f3c3133fb9939a1aa
rustyplaysgamesonps4-ai <rustyplaysgamesonps4@gmail.com>
2026-04-07 04:57:20 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+
+const apiRoutes = require("./routes/api");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
